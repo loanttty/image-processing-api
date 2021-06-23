5f5f986ea5c31c9b41bd985ddf342380a94e1d19
loanttty <loan.tran2802@gmail.com>
2021-06-23 17:59:56 +0800

    Preliminary Resize Image

--- build/index.js
+++ build/index.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log("ahihi");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var sharp_1 = __importDefault(require("sharp"));
+var inputImg = './asset/*.jpg';
+var output = './thumb/resizedImg.jpg';
+var app = express_1.default();
+var port = 3000;
+app.get('/api/image', function (req, res) {
+    res.send('Resizing...');
+    var _a = req.query, title = _a.title, width = _a.width, height = _a.height;
+    sharp_1.default("./asset/" + title + ".jpg")
+        .resize(width, height)
+        .toFile(output)
+        .then(function (img) {
+        res.send('Resizing done!');
+        res.send(img);
+        //fsPromise.writeFile(output,img)
+    });
+});
+app.listen(port, function () {
+    console.log("server starts at http://localhost:" + port);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("ahihi");
+import express from 'express'
+import {promises as fsPromise} from 'fs'
+import sharp from 'sharp'
+import path from 'path'
+
+const app = express()
+const port = 3000
+
+const output = path.join(__dirname,'/thumb/resizedImg.jpg')
+
+app.get('/api/image', (req,res) => {
+    const {title,width,height} = req.query
+    const widthTS = parseInt(width as string)
+    const heightTS = parseInt(height as string)
+
+    sharp(`./asset/${title}.jpg`)
+        .resize(widthTS,heightTS)
+        .toFile(output)
+        .then(() => {
+            res.sendFile(output)
+            //fsPromise.writeFile(output,img)
+        })
+
+})
+
+app.listen(port, () => {
+    console.log(`server starts at http://localhost:${port}`)
+})
