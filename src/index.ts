9e9195754dd38500afbc14c0efa045d7fdcfa404
loanttty <loan.tran2802@gmail.com>
2021-06-25 10:15:24 +0800

    Added save and read file function

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,45 @@
 import express from 'express'
-import {promises as fsPromise} from 'fs'
+import {promises as fsPromises, read} from 'fs'
 import sharp from 'sharp'
 import path from 'path'
 
 const app = express()
 const port = 3000
 
-const output = path.join(__dirname,'/thumb/resizedImg.jpg')
+const storage = path.join(__dirname,`/storage.txt`)
+let imageInfo : Object
+
 
 app.get('/api/image', (req,res) => {
     const {title,width,height} = req.query
+    const titleTS = title as string
     const widthTS = parseInt(width as string)
     const heightTS = parseInt(height as string)
+    const output = path.join(__dirname,`/thumb/resized${title}${widthTS}x${heightTS}.jpg`)
 
-    sharp(`./asset/${title}.jpg`)
+    const readData = async () => {
+        const myFile = await fsPromises.readFile(storage,'utf-8')
+        console.log(JSON.parse(myFile))
+    }
+
+    readData()
+
+    sharp(`./asset/${titleTS}.jpg`)
         .resize(widthTS,heightTS)
         .toFile(output)
-        .then(() => {
+        .then((info) => {
             res.sendFile(output)
-            //fsPromise.writeFile(output,img)
-        })
+            imageInfo = {
+                ...imageInfo,
+                [titleTS]: info
+            }
 
+            const writeData = async () => {
+                const myFile = await fsPromises.open(storage,'a+')
+                await myFile.write(JSON.stringify(imageInfo))
+            }
+            writeData()
+        })
 })
 
 app.listen(port, () => {
