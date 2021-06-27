746ddb20c31b69fa943d93f0aaf972f401a2bcb9
LoanTT <79430764+loanttty@users.noreply.github.com>
2021-06-27 11:59:54 +0800

    Update indexSpec.ts

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,9 +1,8 @@
 import supertest from "supertest"
 import app from "../index"
 
-
 describe('GET /api/jpg', () => {
-    it('should generate new resizing image', (done) => {
+    it('should response with status 200', (done) => {
         supertest(app)
             .get('/api/jpg')
             .expect(200, done);
